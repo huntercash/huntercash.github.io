73b45078dc70b382fc4876f3c372d429158e5352
Hunter Cash <hunter.cash@hlflake.com>
2019-04-22 16:49:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,5 @@ anime({
   translateY:[{value:250, duration: 2000, delay: 1000}],
   // delay: 2000,
   easing: 'easeInOutSine',
-  scale:{ value:2, delay: 3000}
+  scale:{ value:1.4, delay: 3000}
 });
