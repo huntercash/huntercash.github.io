4c8eb73cf4ad6d1e1601047b76e0ca3cc958f2e0
Hunter Cash <hunter.cash@hlflake.com>
2019-04-22 16:46:26 -0500

    Added anime.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+anime({
+  targets: 'h1.helloiam',
+  opacity: [{value: 1, duration: 1000}],
+  translateY:[{value:250, duration: 2000, delay: 1000}],
+  // delay: 2000,
+  easing: 'easeInOutSine',
+  scale:{ value:2, delay: 3000}
+});
