245220d7f8591374a5b05e91a0e99c8e19cd1957
Hunter Cash <hunter.cash@hlflake.com>
2019-05-14 12:03:32 -0500

    Update the D3 Section

--- js/alienapp.js
+++ js/alienapp.js
@@ -1,4 +1,28 @@
-// from data.js
+// Define Global Variables
 var tableData = data;
+var tbody = d3.select("tbody");
+// Extra Data that needs to be added to the dataset. 
+var secondEntry = {
+    datetime: "1/28/1996",
+    city: "dallas",
+    state: "tx",
+    country: "us",
+    shape: "star",
+    durationMinutes: "5 mins.",
+    comments: "Cowboys win a superbowl, that's alien!."
+    };
 
-// YOUR CODE HERE!
+// Add the SecondEntry to the tableData Array
+tableData.push(secondEntry);
+// Grab the Data and make it appear in the javascript.html page
+tableData.forEach((datum) => {
+    // Function to append a table row
+    var row = tbody.append("tr");
+    // Function to append the data to the row
+    Object.entries(datum).forEach(([index, value]) => {
+      var cell = row.append("td").text(value);
+    });
+  });
+
+
+  
