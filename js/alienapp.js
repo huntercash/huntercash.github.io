3751a3326e096e0083c375a3320d7a7616636efb
Hunter Cash <hmclaug@gmail.com>
2019-05-15 21:10:07 -0500

    finalized upload
    search has limited functionality..

--- js/alienapp.js
+++ js/alienapp.js
@@ -3,13 +3,11 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var submit = d3.select("#filter-btn");
 // for use in selecting cities
-var citySelector = d3.select("#filter-city")
+var citySelector = d3.select("#filter-city");
 // for use in selecting states
-var stateSelector = d3.select("#filter-state")
-var countrySelector = d3.select("#filter-country")
-var shapeSelector = d3.select("#filter-shape")
-
-
+var stateSelector = d3.select("#filter-state");
+var countrySelector = d3.select("#filter-country");
+var shapeSelector = d3.select("#filter-shape");
 
 
 
@@ -49,20 +47,45 @@ submit.on("click", function() {
     d3.event.preventDefault();
     // Select the input from the date element and get the raw input
     var inputElement = d3.select("#datetime");
-    var dateValue = inputElement.property("value");
-    // Check if the listening is working on submit click
-    // console.log(this);
-    // console.log(dateValue);
+    var inputCity = d3.select("#selectcity");
+    var inputState= d3.select("#selectstate");
+    var inputCountry = d3.select("#selectcountry");
+    var inputShape = d3.select("#selectshape");
 
-    // Create a Filter Based on Users input for the Date
-    var filteredDate = tableData.filter(tableData => tableData.datetime === dateValue);
+    var dateValue = inputElement.property("value");
+    var cityValue = inputCity.property("value");
+    var stateValue = inputState.property("value");
+    var countryValue = inputCountry.property("value");
+    var shapeValue = inputShape.property("value");
+
+    // Create a Filter Based on Users input for the Date]
+    
+
+
+  var filteredData = tableData.filter(tableData => ((tableData.datetime === dateValue)) || tableData.city === cityValue || tableData.state === stateValue || tableData.country === countryValue || tableData.shape === shapeValue);
+    
+    // WORK IN PROGRESS multi variable filter.. very complicated.. 
+// if (shapeValue || countryValue || stateValue || cityValue !== tableData.shape || tableData.country || tableData.state || tableData.city) {
+//   var filteredData = tableData.filter(tableData => tableData.datetime === dateValue);
+//   console.log("mainIf")
+// }
+// else if (dateValue === tableData.datetime || cityValue === tableData.city) {
+//   console.log("ElseIf1")
+//   var filteredData = tableData.filter(tableData.datetime === dateValue && tableData.city === cityValue);
+// }
+// else {
+//   console.log("else")
+//   var filteredData = tableData.filter(tableData => ((tableData.datetime === dateValue)) || tableData.city === cityValue || tableData.state === stateValue || tableData.country === countryValue || tableData.shape === shapeValue);
+// };
+    
     // console.log(filteredDate);
+
 
     //Clear Current Table
     tbody.selectAll("tr").text("")
     
     // Display New Data
-    filteredDate.forEach((datum) => {
+    filteredData.forEach((datum) => {
         // Function to append a table row
         var row = tbody.append("tr");
         // Function to append the data to the row
@@ -73,182 +96,3 @@ submit.on("click", function() {
 });
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Create Filter Functionality for the city
-
-// Select the submit button
-citySelector.on("click", function() {
-    // Prevent Page from refreshing
-    d3.event.preventDefault();
-    // Select the input from the date element and get the raw input
-    var inputElement = d3.select("#selectcity");
-    var cityValue = inputElement.property("value");
-    var filteredCity = tableData.filter(tableData => tableData.city === cityValue);
-    tbody.selectAll("tr").text("")
-    filteredCity.forEach((datum) => {
-        // Function to append a table row
-        var row = tbody.append("tr");
-        // Function to append the data to the row
-        Object.entries(datum).forEach(([key, value]) => {
-          var cell = row.append("td").text(value);
-        });
-      });
-});
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Create Filter Functionality for the state
-
-// Select the submit button
-stateSelector.on("click", function() {
-    // Prevent Page from refreshing
-    d3.event.preventDefault();
-    // Select the input from the date element and get the raw input
-    var inputElement = d3.select("#selectstate");
-    var stateValue = inputElement.property("value");
-    var filteredCity = tableData.filter(tableData => tableData.state === stateValue);
-    tbody.selectAll("tr").text("")
-    filteredCity.forEach((datum) => {
-        // Function to append a table row
-        var row = tbody.append("tr");
-        // Function to append the data to the row
-        Object.entries(datum).forEach(([key, value]) => {
-          var cell = row.append("td").text(value);
-        });
-      });
-});
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// Create Filter Functionality for the country
-
-// Select the submit button
-countrySelector.on("click", function() {
-    // Prevent Page from refreshing
-    d3.event.preventDefault();
-    // Select the input from the date element and get the raw input
-    var inputElement = d3.select("#selectcountry");
-    var countryValue = inputElement.property("value");
-    var filteredCountry = tableData.filter(tableData => tableData.country === countryValue);
-    tbody.selectAll("tr").text("")
-    filteredCountry.forEach((datum) => {
-        // Function to append a table row
-        var row = tbody.append("tr");
-        // Function to append the data to the row
-        Object.entries(datum).forEach(([key, value]) => {
-          var cell = row.append("td").text(value);
-        });
-      });
-});
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Create Filter Functionality for the shape
-
-// Select the submit button
-shapeSelector.on("click", function() {
-    // Prevent Page from refreshing
-    d3.event.preventDefault();
-    // Select the input from the date element and get the raw input
-    var inputElement = d3.select("#selectshape");
-    var shapeValue = inputElement.property("value");
-    var filteredShape = tableData.filter(tableData => tableData.shape === shapeValue);
-    tbody.selectAll("tr").text("")
-    filteredShape.forEach((datum) => {
-        // Function to append a table row
-        var row = tbody.append("tr");
-        // Function to append the data to the row
-        Object.entries(datum).forEach(([key, value]) => {
-          var cell = row.append("td").text(value);
-        });
-      });
-});
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Create Dropdown Filter Functionality
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Display Unique Cities to put into filter dropdown
-// var citiesList = tableData.map(function(entry){
-//     return entry.city;
-// });
-// // returns a new list of UNIQUE cities to be used in the creation of the dropdown menu
-// var citiesListUnique = d3.set(citiesList).values();
-// var citiesAlphabetical = citiesListUnique.sort();
-// // Display Unique Cities List as the Dropdown
-// var i = 0;
-// citiesAlphabetical.forEach((datum) => {
-//     // Function to append a table row
-//     var item = citySelector.append("a").attr("class", "dropdown-item").attr("id", "cities").text(citiesAlphabetical[i++]);
-//   });
-
-
-// citySelector.on("click", function() {
-// // Prevent Page from refreshing
-//     d3.event.preventDefault();
-//     // Select the input from the date element and get the raw input
-//     var inputElement = d3.select("#cities");
-//     var cityValue = inputElement.property("text");
-//     console.log(cityValue);
-//     // Create a Filter Based on Users input for the Date
-//     var filteredCity = tableData.filter(tableData => tableData.city === cityValue);
-
-    
-//     //Clear Current Table
-//     tbody.selectAll("tr").text("")
-
-//     // Display New Data
-//         filteredCity.forEach((datum) => {
-//         // Function to append a table row
-//         var row = tbody.append("tr");
-//         // Function to append the data to the row
-//         Object.entries(datum).forEach(([key, value]) => {
-//             var cell = row.append("td").text(value);
-//         });
-//     });
-// });
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Display Unique States to put into filter dropdown
-// var stateList = tableData.map(function(entry){
-//     return entry.state;
-// });
-// // returns a new list of UNIQUE States to be used in the creation of the dropdown menu
-// var stateListUnique = d3.set(stateList).values();
-// var stateAlphabetical = stateListUnique.sort();
-// // Display Unique States List as the Dropdown
-// var g = 0;
-// stateAlphabetical.forEach((datum) => {
-//     // Function to append a table row
-//     var item = stateSelector.append("a").attr("class", "dropdown-item").attr("id", "state").text(stateAlphabetical[g++]);
-//   });
-// stateSelector.on("click", function() {
-//     // Prevent Page from refreshing
-//     d3.event.preventDefault();
-//     // Select the input from the date element and get the raw input
-//     var inputElement = d3.select("#state");
-//     var stateValue = inputElement.property("text");
-//     console.log(stateValue)
-//     // Create a Filter Based on Users input for the Date
-//     var filteredState = tableData.filter(tableData => tableData.state === stateValue);
-//     //Clear Current Table
-//     tbody.selectAll("tr").text("")
-    
-//     // Display New Data
-//     filteredState.forEach((datum) => {
-//         // Function to append a table row
-//         var row = tbody.append("tr");
-//         // Function to append the data to the row
-//         Object.entries(datum).forEach(([key, value]) => {
-//           var cell = row.append("td").text(value);
-//         });
-//       });
-// });
